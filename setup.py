f3f6d35e1b7262de47188c5bc7a6f0ab819c99b9
ebonnal <bonnal.enzo.dev@gmail.com>
2024-10-12 17:38:11 +0100

    1.0.2: add long descr

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup  # type: ignore
 
 setup(
     name="pycout",
-    version="1.0.1",
+    version="1.0.2",
     packages=["pycout"],
-    url="http://github.com/EnzoBnl/pycout",
+    url="http://github.com/ebonnal/pycout",
     license="Apache 2.",
     author="Enzo Bonnal",
-    author_email="enzobonnal@gmail.com",
-    description="""C++'s `cout << "Hello" << " World !" << endl;` in Python.""",
+    author_email="bonnal.enzo.dev@gmail.com",
+    description="C++'s cout for Python ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
